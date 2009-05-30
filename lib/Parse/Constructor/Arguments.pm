0a87b1aa4042306d94c116832d5f5113592e0faa
nperez <nperez@cpan.org>
2009-05-29 19:05:54 -0500

    boba tea shop

--- lib/Parse/Constructor/Arguments.pm
+++ lib/Parse/Constructor/Arguments.pm
@@ -2,47 +2,185 @@ package Parse::Constructor::Arguments;
 
 use 5.010;
 
-use PPI;
 use PPI::Dumper;
 
-sub parse
+use MooseX::Declare;
+
+class Parse::Constructor::Arguments
 {
-    my ($class, $str) = @_;
+    use PPI;
+    use MooseX::Types::Moose(':all');
     
-    my $doc = PPI::Document->new(\$str);
-    my %data;
+    has document =>
+    (
+        is          => 'ro',
+        isa         => 'PPI::Document',
+        lazy_build  => 1,
+    );
 
-    while(1)
+    has current =>
+    (
+        is          => 'ro',
+        isa         => 'PPI::Node',
+        lazy        => 1,
+        writer      => '_set_current',
+    );
+
+    has input   =>
+    (
+        is          => 'ro'
+        isa         => Str,
+        required    => 1,
+    );
+
+    method _build_document()
     {
-        my $token;
-        eval { $token = get_next_significant($doc) };
-        return undef if $@;
-        
-        if($token->isa('PPI::Token::Word'))
-        {
-            my $sub;
-            eval { $sub = get_next_significant($doc) };
-            return undef if $@;
+        my $document = PPI::Document->new(\{$self->input});
+        $document->add_element(PPI::Statement::Null->new(';'));
+        $sef->get_first_significant_token;
+    }
 
-            if($sub->isa('PPI::Structure::Constructor'))
+    method parse(ClassName $class: Str $str)
+    {
+        my $self = $class->new(input => %str);
+        
+        my %data;
+        while(1)
+        {
+            my $token = $self->current;
+
+            say "token: $token";
+            
+            if($token->isa('PPI::Token::Word'))
             {
-                say "$sub";
+                say "word: $token";
+                my $key = $token->content;
+                
+                $data{$key} = undef;
+
+                $token = $self->get_next_significant;
+
+                if($token->isa('PPI::Token::Operator') && $token->content =~ /,|(?:=>)/)
+                {
+                    say "comma: $token";
+
+                    $token = $self->get_next_significant;
+                    
+                    if($token->parent->isa('PPI::Structure::Constructor'))
+                    {
+                        say "constructor: $token";
+                        
+                        $data{$key} = $self->process;
+                    }
+                }
+            }
+            elsif($token->parent->isa('PPI::Statement::Null'))
+            {
                 last;
             }
         }
-    }
-}
 
-sub get_next_significant
-{
-    my $doc = shift;
-    
-    while(1)
+        return \%data;
+    }
+
+    method process()
     {
-        my $token = $doc->next_token;
-        die 'No more significant tokens in stream' if not $token;
-        next if !$token->significant;
-        return $token;
+        my $data;
+        my $applicator;
+        my $terminator;
+        my $current = $self->current;
+        
+        if($current->content eq '[')
+        {
+            $data = [];
+            $terminator = ']';
+            $applicator = sub { push(@$_[0], $_[2]) };
+        }
+        elsif($current->content eq '{')
+        {
+            $data = {};
+            $terminator = '}';
+            $applicator = sub { $_[0]->{$_[1]} = $_[2] };
+        }
+
+        my $token = $self->get_next_significant;
+        my $word;
+        my $prev;
+
+        while($token->content ne $terminator)
+        {
+            my $class = $token->class;
+
+            if($class eq 'PPI::Token::Word')
+            {
+                $word = $token->content;
+                $token = $self->get_next_significant;
+                $class = $token->class;
+            }
+
+            if($class eq 'PPI::Token::Number')
+            {
+                $applicator->($data, $word, $token->content);
+                $word = undef;
+            }
+            elsif($class eq 'PPI::Token::Structure')
+            {
+                $applicator->($data, $word, $self->process);
+                $word = undef;
+            }
+            elsif($class eq 'PPI::Token::Quote')
+            {
+                die 'Double quoted or interpolated strings are not supported'
+                    if $token->isa('PPI::Token::Quote::Double') or
+                    if $token->isa('PPI::Token::Quote::Interpolated');
+                
+                $applicator->($data, $word, $token->content);
+                $word = undef;
+
+            }
+            elsif($class eq 'PPI::Token::Operator')
+            {
+                if($token->content =~ /,|=>/)
+                {
+                    next;
+                }
+            }
+
+            $applicator->($data, undef, $word) if $word;
+            $word = undef;
+        }
+
+        return $array;
+    }
+
+    method get_next_significant()
+    {
+        while(1)
+        {
+            $token = $self->current->next_token;
+            die "No more significant tokens in stream: '$token'" if not $token;
+            
+            if(!$token->significant)
+            {
+                next;
+            }
+
+            $self->_set_current($token);
+            return $token;
+        }
+    }
+
+    method get_first_signficant_token()
+    {
+        my $token = $self->document->first_token;
+        
+        if($token->significant)
+        {
+            $self->_set_current($token);
+            return $token;
+        }
+        
+        return $self->get_next_significant($token);
     }
 }
 
